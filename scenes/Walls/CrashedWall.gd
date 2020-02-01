c1547f255d315b92c40704c2816d1c2d8d26e6ae
Nacho <ignacioabadie@live.com.ar>
2020-02-01 10:50:50 -0300

    kickoff fix-it, manitas

---
+++ scenes/Walls/CrashedWall.gd
@@ -0,0 +1,35 @@
+extends Node2D
+
+# Wall exported method for interface builder
+export var MAX_WALL_LIFE: int = 200;
+export var initial_value: int = 100;
+
+# Wall constants
+const NO_LIFE: int = 0;
+
+# Wall variables
+var wall_life: int = initial_value;
+
+# Wall processing
+func _ready():
+	$LifeBar.max_value = MAX_WALL_LIFE
+	$LifeBar.value = initial_value
+	pass
+
+func _process(delta):
+	$LifeBar.value = wall_life;
+	# TODO - inputs added for debuggin
+	if Input.is_action_just_pressed("a"):
+		crash_wall()
+	if Input.is_action_just_pressed("s"):
+		fix_wall()
+	## END TODO
+
+# Wall life methods
+func fix_wall(amount: int = 10):
+	if wall_life < MAX_WALL_LIFE:
+		wall_life += amount
+
+func crash_wall(amount: int = 10):
+	if wall_life > NO_LIFE:
+		wall_life -= amount
