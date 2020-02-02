4deed0d2c0797242b282d14d8c47f7eb1c341c0e
Nacho <ignacioabadie@live.com.ar>
2020-02-02 11:11:32 -0300

    added menu

---
+++ scenes/Menu/StartButton.gd
@@ -0,0 +1,26 @@
+extends Node2D
+
+export (String) var label_text = "Press";
+export (int, 1, 2) var player_number = 1;
+
+var can_select: bool = true
+
+func _ready():
+	$ButtonLabel.text = label_text;
+	$Animation.play("binding_label");
+	$PlayerReady.visible = false;
+	pass
+	
+func _process(delta):
+	if can_select && Input.is_action_just_pressed("Action"):
+		selection_ready();
+
+func selection_ready():
+	$Animation.stop();
+	$Animation.play("selected");
+	$ButtonLabel.visible = false;
+	$PlayerReady.visible = true;
+	$PlayerReady/ReadyText.text = "P" + str(player_number) + " READY!";
+
+func set_font():
+	$ButtonLabel
